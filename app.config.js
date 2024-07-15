3957f075e0b97dbb17c6c8148f7db55326db19c7
iazadur <iamazadur@gmail.com>
2024-07-15 15:49:14 +0600

    fix: base url issue

--- app.config.js
+++ app.config.js
@@ -1,6 +1,7 @@
 // export const API_ACCESS_TOKEN =
-//   process.env.NEXT_PUBLIC_MAP_API_ACCESS_TOKEN || "";
+// process.env.NEXT_PUBLIC_MAP_API_ACCESS_TOKEN || "";
 
 export const API = {
-  OCR_API: `https://scraping.barikoimaps.dev/bank/application/information`,
+  // OCR_API: `https://scraping.barikoimaps.dev/bank/application/information`,
+  OCR_API: `/api/bank/application/information`,
 };
